0ad1e2fe499a741660bb759ed15f3eb0a4e37b2d
Marco Brescianini <marco.brescianini@kaleyra.com>
2023-06-28 16:03:48 +0200

    Update Package.swift replacing literals with named constants

--- Package.swift
+++ Package.swift
@@ -2,6 +2,14 @@
 
 import PackageDescription
 
+let sdkName = "Bandyer"
+let sdkVersion = "3.7.0"
+let sdkChecksum = "b837c136a897e7590b765d07b80b672546d26e72973510ba9013bb988a9ad4bd"
+
+let rtcName = "WebRTC"
+let rtcVersion = "100.2.36026"
+let rtcChecksum = "ebfd70982677c71f3ac46f721d0566b5f41d7d6619f29818c7e049e8768f83c1"
+
 let package = Package(
     name: "Bandyer",
     platforms: [
@@ -14,12 +22,12 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "Bandyer",
-            url: "https://static.bandyer.com/corporate/iOS/releases/sdk/3.7.0/standalone/Bandyer.xcframework.zip",
-            checksum: "b837c136a897e7590b765d07b80b672546d26e72973510ba9013bb988a9ad4bd"),
+            name: sdkName,
+            url: "https://static.bandyer.com/corporate/iOS/releases/sdk/\(sdkVersion)/standalone/Bandyer.xcframework.zip",
+            checksum: sdkChecksum),
         .binaryTarget(
-            name: "WebRTC",
-            url: "https://static.bandyer.com/corporate/iOS/releases/webrtc/100.2.36026/WebRTC.xcframework.zip",
-            checksum: "ebfd70982677c71f3ac46f721d0566b5f41d7d6619f29818c7e049e8768f83c1")
+            name: rtcName,
+            url: "https://static.bandyer.com/corporate/iOS/releases/webrtc/\(rtcVersion)/WebRTC.xcframework.zip",
+            checksum: rtcChecksum)
     ]
 )
