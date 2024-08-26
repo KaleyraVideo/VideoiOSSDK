47d0976e0f68a6021b6256038b58410e816e5234
Marco Brescianini <marco.brescianini@kaleyra.com>
2024-08-26 16:31:31 +0200

    Update package swift in preparation for 4.0 pre-release

--- Package.swift
+++ Package.swift
@@ -2,32 +2,31 @@
 
 import PackageDescription
 
-let sdkName = "Bandyer"
+let sdkName = "KaleyraVideoSDK"
 let sdkVersion = "3.12.1"
 let sdkChecksum = "a310a8abd01d1b6d35f35ef15b789b697395b29ebe8405d6f653bc45ba7d0d10"
 
 let rtcName = "WebRTC"
-let rtcVersion = "100.2.36026"
-let rtcChecksum = "ebfd70982677c71f3ac46f721d0566b5f41d7d6619f29818c7e049e8768f83c1"
+let rtcChecksum = "2e9f011ed68781ec238c73b5515eaae4d5b33b4d92f60940c9abe72759cfc646"
 
 let package = Package(
-    name: "Bandyer",
+    name: sdkName,
     platforms: [
         .iOS(.v10)
     ],
     products: [
         .library(
-            name: "Bandyer",
-            targets: ["Bandyer", "WebRTC"])
+            name: sdkName,
+            targets: [sdkName, rtcName])
     ],
     targets: [
         .binaryTarget(
             name: sdkName,
-            url: "https://static.bandyer.com/corporate/iOS/releases/sdk/\(sdkVersion)/standalone/Bandyer.xcframework.zip",
+            url: "https://static.bandyer.com/corporate/iOS/releases/sdk/\(sdkVersion)/standalone/KaleyraVideoSDK.xcframework.zip",
             checksum: sdkChecksum),
         .binaryTarget(
             name: rtcName,
-            url: "https://static.bandyer.com/corporate/iOS/releases/webrtc/\(rtcVersion)/WebRTC.xcframework.zip",
+            url: "https://github.com/KaleyraVideo/WebRTC/releases/download/124.0.0/WebRTC-M124.xcframework.zip",
             checksum: rtcChecksum)
     ]
 )
